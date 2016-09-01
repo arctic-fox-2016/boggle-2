24011f3ba5a21cbbea303d7428c916a895995238
Dash O'Pepper & Kun Anggiar Lanang &   "name": null <pair+-.kun7.tevin@hacktiv8.com>
2016-09-01 21:25:06 +0700

    Tevinstein & Kun

--- boggle.js
+++ boggle.js
@@ -1,15 +1,19 @@
+var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var i;
+var j;
+var huruf = [];
+
 function randomize(){
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var i;
-    var j;
-    var huruf = [];
+  for(i=0; i<16; i++){
+    huruf.push(possible.charAt(Math.floor(Math.random() * possible.length)));
+    console.log(huruf[i])
+  }
 
-    for(i=0; i<16; i++){
-      huruf.push(possible.charAt(Math.floor(Math.random() * possible.length)));
-      console.log(huruf[i])
-    }
+  for(j=0; j<huruf.length; j++){
+    document.getElementsByClassName("kotak")[j].innerHTML = huruf[j];
+  }
+}
 
-    for(j=0; j<huruf.length; j++){
-      document.getElementsByClassName("kotak")[j].innerHTML = huruf[j];
-    }
+function solve(){
+
 }
